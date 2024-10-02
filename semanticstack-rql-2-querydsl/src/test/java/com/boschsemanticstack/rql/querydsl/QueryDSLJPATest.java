8a1c9dc5af33b5f109c1132512f2155a66257ecc
Dennis Melzer <dennis.melzer@de.bosch.com>
2024-10-02 16:35:18 +0200

    Add surefire plugin

--- semanticstack-rql-2-querydsl/src/test/java/com/boschsemanticstack/rql/querydsl/QueryDSLJPATest.java
+++ semanticstack-rql-2-querydsl/src/test/java/com/boschsemanticstack/rql/querydsl/QueryDSLJPATest.java
@@ -29,33 +29,33 @@ import com.querydsl.jpa.impl.JPAQuery;
 import org.junit.jupiter.api.Test;
 
 class QueryDSLJPATest {
+ 
+  @Test
+  void rawQueryDSLComparisonWithConstraintOnCollectionEntryAndItsSupplementsShouldReturnNothing() {
+    final JPQLSubQuery<Entity> waySubQuery = JPAExpressions.selectFrom(QEntity.entity)
+        .leftJoin(QEntity.entity.subEntities, QSubEntity.subEntity)
+        .leftJoin(QEntity.entity.subEntities2);
+    final Predicate innerPredicate = new BooleanBuilder()
+        .and(QSubEntity.subEntity.name.eq("a"))
+        .and(QSubEntity.subEntity.metadata.contains("mkey", "mvalue"))
+        .and(QSubEntity2.subEntity2.name.eq("b"))
+        .getValue();
+    waySubQuery.where(innerPredicate);
+    final Predicate predicate = QEntity.entity.in(waySubQuery);
+    final String jpaQuery = asJpaQuery(predicate);
+    assertThat(jpaQuery).isEqualTo("\n"
+        + "where entity in (select entity\n"
+        + "from Entity entity\n"
+        + "  left join entity.subEntities as subEntity\n"
+        + "  left join entity.subEntities2\n"
+        + "  left join subEntity.metadata as subEntity_metadata_0 on key(subEntity_metadata_0) = ?1\n"
+        + "where subEntity.name = ?2 and subEntity_metadata_0 = ?3 and subEntity2.name = ?4)");
+  }
 
-   @Test
-   void rawQueryDSLComparisonWithConstraintOnCollectionEntryAndItsSupplementsShouldReturnNothing() {
-      final JPQLSubQuery<Entity> waySubQuery = JPAExpressions.selectFrom( QEntity.entity )
-            .leftJoin( QEntity.entity.subEntities, QSubEntity.subEntity )
-            .leftJoin( QEntity.entity.subEntities2 );
-      final Predicate innerPredicate = new BooleanBuilder()
-            .and( QSubEntity.subEntity.name.eq( "a" ) )
-            .and( QSubEntity.subEntity.metadata.contains( "mkey", "mvalue" ) )
-            .and( QSubEntity2.subEntity2.name.eq( "b" ) )
-            .getValue();
-      waySubQuery.where( innerPredicate );
-      final Predicate predicate = QEntity.entity.in( waySubQuery );
-      final String jpaQuery = asJpaQuery( predicate );
-      assertThat( jpaQuery ).isEqualTo( "\n"
-            + "where entity in (select entity\n"
-            + "from Entity entity\n"
-            + "  left join entity.subEntities as subEntity\n"
-            + "  left join entity.subEntities2\n"
-            + "  left join subEntity.metadata as subEntity_metadata_0 on key(subEntity_metadata_0) = ?1\n"
-            + "where subEntity.name = ?2 and subEntity_metadata_0 = ?3 and subEntity2.name = ?4)" );
-   }
-
-   private String asJpaQuery( final Predicate predicate ) {
-      final JPQLSerializer serializer = new JPQLSerializer( JPQLTemplates.DEFAULT );
-      final JPAQuery jpaQuery = (JPAQuery) new JPAQuery().where( predicate );
-      serializer.serialize( jpaQuery.getMetadata(), false, null );
-      return serializer.toString();
-   }
+  private String asJpaQuery(final Predicate predicate) {
+    final JPQLSerializer serializer = new JPQLSerializer(JPQLTemplates.DEFAULT);
+    final JPAQuery jpaQuery = (JPAQuery) new JPAQuery().where(predicate);
+    serializer.serialize(jpaQuery.getMetadata(), false, null);
+    return serializer.toString();
+  }
 }
