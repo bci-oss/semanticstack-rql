274ff1d8d11661a7c615a5418d206b5ecad6edf8
Dennis Melzer <dennis.melzer@de.bosch.com>
2024-10-02 16:42:42 +0200

    Fix code style

--- semanticstack-rql-2-querydsl/src/main/java/com/boschsemanticstack/rql/querydsl/resolvers/JpaCollectionPathResolver.java
+++ semanticstack-rql-2-querydsl/src/main/java/com/boschsemanticstack/rql/querydsl/resolvers/JpaCollectionPathResolver.java
@@ -13,6 +13,13 @@
 
 package com.boschsemanticstack.rql.querydsl.resolvers;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Queue;
+
 import com.boschsemanticstack.rql.model.v1.RqlFilter;
 import com.querydsl.core.types.CollectionExpression;
 import com.querydsl.core.types.EntityPath;
@@ -22,88 +29,82 @@ import com.querydsl.core.types.dsl.EntityPathBase;
 import com.querydsl.core.types.dsl.SimpleExpression;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLSubQuery;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Queue;
-
+ 
 /**
  * JPA-specific version of the {@link CollectionPathResolver} that takes care of properly created
  * queries in order to keep join-semantic for multiple conditions on collection entries. This means
  * all predicats will be applied to the same collection entry.
  */
-@SuppressWarnings({"java:S3740"})
+@SuppressWarnings( { "java:S3740" } )
 // java:S3740 parameterized types - thy are not known for the expressions handled here so they cannot be given explicitely
 public class JpaCollectionPathResolver<T> extends RecursiveResolver<CollectionPathBase<?, ?, ?>> {
 
-  private final SimpleValueResolver simpleValueResolver;
+   private final SimpleValueResolver simpleValueResolver;
 
-  private final Map<CollectionExpression, EntityPath> subCollections = new LinkedHashMap<>();
-  private final EntityPathBase<T> rootResource;
+   private final Map<CollectionExpression, EntityPath> subCollections = new LinkedHashMap<>();
+   private final EntityPathBase<T> rootResource;
 
-  public JpaCollectionPathResolver(final AbstractPathPredicateResolver parentResolver,
-      final EntityPathBase<T> rootResource,
-      final SimpleValueResolver simpleValueResolver) {
-    super(parentResolver);
-    this.rootResource = rootResource;
-    this.simpleValueResolver = simpleValueResolver;
-  }
+   public JpaCollectionPathResolver( final AbstractPathPredicateResolver parentResolver,
+         final EntityPathBase<T> rootResource,
+         final SimpleValueResolver simpleValueResolver ) {
+      super( parentResolver );
+      this.rootResource = rootResource;
+      this.simpleValueResolver = simpleValueResolver;
+   }
 
-  @Override
-  public Predicate resolve(final SimpleExpression root, final Field field,
-      final Queue<String> pathElements, final RqlFilter filter)
-      throws ReflectiveOperationException {
-    return resolveCollection((CollectionPathBase) field.get(root), pathElements, filter);
-  }
+   @Override
+   public Predicate resolve( final SimpleExpression root, final Field field,
+         final Queue<String> pathElements, final RqlFilter filter )
+         throws ReflectiveOperationException {
+      return resolveCollection( (CollectionPathBase) field.get( root ), pathElements, filter );
+   }
 
-  @Override
-  public Predicate resolveMethod(final SimpleExpression root, final Method method,
-      final Queue<String> pathElements, final RqlFilter filter)
-      throws ReflectiveOperationException {
-    return resolveCollection((CollectionPathBase) method.invoke(root), pathElements, filter);
-  }
+   @Override
+   public Predicate resolveMethod( final SimpleExpression root, final Method method,
+         final Queue<String> pathElements, final RqlFilter filter )
+         throws ReflectiveOperationException {
+      return resolveCollection( (CollectionPathBase) method.invoke( root ), pathElements, filter );
+   }
 
-  public Predicate resolveCollection(final CollectionPathBase collectionPath,
-      final Queue<String> pathElements, final RqlFilter filter)
-      throws ReflectiveOperationException {
+   public Predicate resolveCollection( final CollectionPathBase collectionPath,
+         final Queue<String> pathElements, final RqlFilter filter )
+         throws ReflectiveOperationException {
 
-    final SimpleExpression anyCollectionEntryPath = collectionPath.any();
-    if (pathElements.isEmpty()) {
-      return simpleValueResolver.resolve(anyCollectionEntryPath, null, pathElements, filter);
-    } else {
-      final Class<? extends SimpleExpression> typeOfCollectionEntries = anyCollectionEntryPath.getClass();
-      final Field subentityQInstanceField = Arrays.stream(
-              typeOfCollectionEntries.getDeclaredFields())
-          .filter(entityField -> entityField.getType()
-              .equals(
-                  typeOfCollectionEntries))
-          .findFirst().orElseThrow(
-              () -> new RuntimeException("Invalid collection entry type."));
-      final EntityPath<?> collectionEntryPath = (EntityPath) subentityQInstanceField.get(null);
+      final SimpleExpression anyCollectionEntryPath = collectionPath.any();
+      if ( pathElements.isEmpty() ) {
+         return simpleValueResolver.resolve( anyCollectionEntryPath, null, pathElements, filter );
+      } else {
+         final Class<? extends SimpleExpression> typeOfCollectionEntries = anyCollectionEntryPath.getClass();
+         final Field subentityQInstanceField = Arrays.stream(
+                     typeOfCollectionEntries.getDeclaredFields() )
+               .filter( entityField -> entityField.getType()
+                     .equals(
+                           typeOfCollectionEntries ) )
+               .findFirst().orElseThrow(
+                     () -> new RuntimeException( "Invalid collection entry type." ) );
+         final EntityPath<?> collectionEntryPath = (EntityPath) subentityQInstanceField.get( null );
 
-      subCollections.put(collectionPath, collectionEntryPath);
-      return super.resolve((SimpleExpression) collectionEntryPath, null, pathElements, filter);
-    }
-  }
+         subCollections.put( collectionPath, collectionEntryPath );
+         return super.resolve( (SimpleExpression) collectionEntryPath, null, pathElements, filter );
+      }
+   }
 
-  @Override
-  public Predicate postProcess(final Predicate predicate) {
-    if (!subCollections.isEmpty()) {
-      final JPQLSubQuery<T> subQuery = JPAExpressions.selectFrom(rootResource);
-      subCollections.forEach(
-          (collectionPath, collectionEntryPath) -> addJoin(subQuery, collectionPath,
-              collectionEntryPath));
-      subQuery.where(predicate);
-      return rootResource.in(subQuery);
-    }
-    return predicate;
-  }
+   @Override
+   public Predicate postProcess( final Predicate predicate ) {
+      if ( !subCollections.isEmpty() ) {
+         final JPQLSubQuery<T> subQuery = JPAExpressions.selectFrom( rootResource );
+         subCollections.forEach(
+               ( collectionPath, collectionEntryPath ) -> addJoin( subQuery, collectionPath,
+                     collectionEntryPath ) );
+         subQuery.where( predicate );
+         return rootResource.in( subQuery );
+      }
+      return predicate;
+   }
 
-  private <P> void addJoin(final JPQLSubQuery<T> subQuery,
-      final CollectionExpression<?, P> collectionPath,
-      final EntityPath<P> collectionEntryPath) {
-    subQuery.leftJoin(collectionPath, collectionEntryPath);
-  }
+   private <P> void addJoin( final JPQLSubQuery<T> subQuery,
+         final CollectionExpression<?, P> collectionPath,
+         final EntityPath<P> collectionEntryPath ) {
+      subQuery.leftJoin( collectionPath, collectionEntryPath );
+   }
 }
--- semanticstack-rql-parser/src/test/java/com/boschsemanticstack/rql/language/RqlRepresentationTest.java
+++ semanticstack-rql-parser/src/test/java/com/boschsemanticstack/rql/language/RqlRepresentationTest.java
@@ -28,65 +28,65 @@ import org.junit.jupiter.api.Test;
 
 class RqlRepresentationTest {
 
-  @Test
-  void queryWithSlashAttributeShouldBeNotParseable() {
-    final String expression = " option=sort(+att1,-att2)"
-        + "&filter=and(eq(att2,\"theSame\"),lt(att1,5),gt(att1,42))"
-        + "&select=att1,att2,att3/subAtt4"
-        + "&option=limit(5,500)";
+   @Test
+   void queryWithSlashAttributeShouldBeNotParseable() {
+      final String expression = " option=sort(+att1,-att2)"
+            + "&filter=and(eq(att2,\"theSame\"),lt(att1,5),gt(att1,42))"
+            + "&select=att1,att2,att3/subAtt4"
+            + "&option=limit(5,500)";
 
-    assertThatThrownBy(() -> RqlParser.from(expression)).isInstanceOf(ParseException.class)
-        .hasMessageContaining("token recognition error at: '/'");
-  }
+      assertThatThrownBy( () -> RqlParser.from( expression ) ).isInstanceOf( ParseException.class )
+            .hasMessageContaining( "token recognition error at: '/'" );
+   }
 
-  @Test
-  void queryWithRandomizedOrderShouldBeParsable() {
-    final String expression = " option=sort(+att1,-att2)" +
-        "&filter=and(eq(att2,\"theSame\"),lt(att1,5),gt(att1,42))" +
-        "&select=att1,att2,att3.subAtt4"
-        + "&option=limit(5,500)";
+   @Test
+   void queryWithRandomizedOrderShouldBeParsable() {
+      final String expression = " option=sort(+att1,-att2)"
+            + "&filter=and(eq(att2,\"theSame\"),lt(att1,5),gt(att1,42))"
+            + "&select=att1,att2,att3.subAtt4"
+            + "&option=limit(5,500)";
 
-    final RqlQueryModel query = RqlParser.from(expression);
+      final RqlQueryModel query = RqlParser.from( expression );
 
-    assertThat(query.getOptions().getOrder().fieldDirections())
-        .containsExactly(
-            new RqlFieldDirectionImpl("att1", RqlFieldDirection.Direction.ASCENDING),
-            new RqlFieldDirectionImpl("att2", RqlFieldDirection.Direction.DESCENDING)
-        );
-    assertThat(query.getOptions().getSlice().get().limit()).isEqualTo(500);
-    assertThat(query.getOptions().getSlice().get().offset()).isEqualTo(5);
-    assertThat(query.getSelect().attributes())
-        .containsExactly("att1", "att2", "att3.subAtt4");
+      assertThat( query.getOptions().getOrder().fieldDirections() )
+            .containsExactly(
+                  new RqlFieldDirectionImpl( "att1", RqlFieldDirection.Direction.ASCENDING ),
+                  new RqlFieldDirectionImpl( "att2", RqlFieldDirection.Direction.DESCENDING )
+            );
+      assertThat( query.getOptions().getSlice().get().limit() ).isEqualTo( 500 );
+      assertThat( query.getOptions().getSlice().get().offset() ).isEqualTo( 5 );
+      assertThat( query.getSelect().attributes() )
+            .containsExactly( "att1", "att2", "att3.subAtt4" );
 
-    assertThat(query.getFilter().get().getChildren()).extracting(RqlFilter::getOperator,
-            RqlFilter::getAttribute, RqlFilter::getValue)
-        .containsExactly(
-            tuple(RqlFilter.Operator.EQ, "att2", "theSame"),
-            tuple(RqlFilter.Operator.LT, "att1", 5),
-            tuple(RqlFilter.Operator.GT, "att1", 42)
+      assertThat( query.getFilter().get().getChildren() ).extracting( RqlFilter::getOperator,
+                  RqlFilter::getAttribute, RqlFilter::getValue )
+            .containsExactly(
+                  tuple( RqlFilter.Operator.EQ, "att2", "theSame" ),
+                  tuple( RqlFilter.Operator.LT, "att1", 5 ),
+                  tuple( RqlFilter.Operator.GT, "att1", 42 )
 
-        );
-  }
+            );
+   }
 
-  @Test
-  void shouldParseNewSyntaxWithSelectEqualsWithoutParens() {
-    // GIVEN
-    final String expression = "select=id,name&filter=eq(id,\"4711\")&option=sort(+name,-description),limit(5,10)";
+   @Test
+   void shouldParseNewSyntaxWithSelectEqualsWithoutParens() {
+      // GIVEN
+      final String expression = "select=id,name&filter=eq(id,\"4711\")&option=sort(+name,-description),limit(5,10)";
 
-    // WHEN
-    final RqlQueryModel model = RqlParser.from(expression);
+      // WHEN
+      final RqlQueryModel model = RqlParser.from( expression );
 
-    // THEN
-    assertThat(model.getSelect().attributes()).containsExactly("id", "name");
+      // THEN
+      assertThat( model.getSelect().attributes() ).containsExactly( "id", "name" );
 
-    assertThat(model.getFilter().get().getOperator()).isEqualTo(RqlFilter.Operator.EQ);
-    assertThat(model.getFilter().get().getValues()).containsExactly("4711");
+      assertThat( model.getFilter().get().getOperator() ).isEqualTo( RqlFilter.Operator.EQ );
+      assertThat( model.getFilter().get().getValues() ).containsExactly( "4711" );
 
-    assertThat(model.getOptions().getOrder().fieldDirections())
-        .containsExactly(
-            new RqlFieldDirectionImpl("name", RqlFieldDirection.Direction.ASCENDING),
-            new RqlFieldDirectionImpl("description", RqlFieldDirection.Direction.DESCENDING)
-        );
-    assertThat(model.getOptions().getSlice()).contains(new RqlSliceImpl(5, 10));
-  }
+      assertThat( model.getOptions().getOrder().fieldDirections() )
+            .containsExactly(
+                  new RqlFieldDirectionImpl( "name", RqlFieldDirection.Direction.ASCENDING ),
+                  new RqlFieldDirectionImpl( "description", RqlFieldDirection.Direction.DESCENDING )
+            );
+      assertThat( model.getOptions().getSlice() ).contains( new RqlSliceImpl( 5, 10 ) );
+   }
 }
