f4fde6c938aa20eb655e5584469290ec2d73de8f
Dennis Melzer <dennis.melzer@de.bosch.com>
2026-03-25 10:25:58 +0100

    add more test

---
+++ examples/semanticstack-rql-examples-querydsljpa/src/test/java/com/boschsemanticstack/rql/examples/querydsljpa/model/BurgerTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) 2024 Robert Bosch Manufacturing Solutions GmbH
+ *
+ * See the AUTHORS file(s) distributed with this work for additional
+ * information regarding authorship.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+package com.boschsemanticstack.rql.examples.querydsljpa.model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class BurgerTest {
+
+   @Test
+   void shouldCreateBurgerWithDefaultConstructor() {
+      final Burger burger = new Burger();
+      assertThat( burger.getId() ).isZero();
+      assertThat( burger.getName() ).isNull();
+      assertThat( burger.getDressing() ).isNull();
+   }
+
+   @Test
+   void shouldCreateBurgerWithAllArgsConstructor() {
+      final Burger burger = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      assertThat( burger.getId() ).isEqualTo( 1L );
+      assertThat( burger.getName() ).isEqualTo( "TestBurger" );
+      assertThat( burger.getDressing() ).isEqualTo( Dressing.MUSTARD );
+   }
+
+   @Test
+   void shouldSetId() {
+      final Burger burger = new Burger();
+      burger.setId( 42L );
+      assertThat( burger.getId() ).isEqualTo( 42L );
+   }
+
+   @Test
+   void shouldSetName() {
+      final Burger burger = new Burger();
+      burger.setName( "CheeseBurger" );
+      assertThat( burger.getName() ).isEqualTo( "CheeseBurger" );
+   }
+
+   @Test
+   void shouldSetDressing() {
+      final Burger burger = new Burger();
+      burger.setDressing( Dressing.CHILI_CHEESE_SAUCE );
+      assertThat( burger.getDressing() ).isEqualTo( Dressing.CHILI_CHEESE_SAUCE );
+   }
+
+   @Test
+   void shouldBeEqualWhenSameValues() {
+      final Burger burger1 = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      final Burger burger2 = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      assertThat( burger1 ).isEqualTo( burger2 );
+   }
+
+   @Test
+   void shouldBeEqualToSelf() {
+      final Burger burger = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      assertThat( burger ).isEqualTo( burger );
+   }
+
+   @Test
+   void shouldNotBeEqualToNull() {
+      final Burger burger = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      assertThat( burger ).isNotEqualTo( null );
+   }
+
+   @Test
+   void shouldNotBeEqualToDifferentClass() {
+      final Burger burger = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      assertThat( burger ).isNotEqualTo( "not a burger" );
+   }
+
+   @Test
+   void shouldNotBeEqualWhenDifferentId() {
+      final Burger burger1 = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      final Burger burger2 = new Burger( 2L, "TestBurger", Dressing.MUSTARD );
+      assertThat( burger1 ).isNotEqualTo( burger2 );
+   }
+
+   @Test
+   void shouldNotBeEqualWhenDifferentName() {
+      final Burger burger1 = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      final Burger burger2 = new Burger( 1L, "OtherBurger", Dressing.MUSTARD );
+      assertThat( burger1 ).isNotEqualTo( burger2 );
+   }
+
+   @Test
+   void shouldNotBeEqualWhenDifferentDressing() {
+      final Burger burger1 = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      final Burger burger2 = new Burger( 1L, "TestBurger", Dressing.TOMATO_SAUCE );
+      assertThat( burger1 ).isNotEqualTo( burger2 );
+   }
+
+   @Test
+   void shouldHaveSameHashCodeWhenEqual() {
+      final Burger burger1 = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      final Burger burger2 = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      assertThat( burger1.hashCode() ).isEqualTo( burger2.hashCode() );
+   }
+
+   @Test
+   void shouldHaveDifferentHashCodeWhenNotEqual() {
+      final Burger burger1 = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      final Burger burger2 = new Burger( 2L, "OtherBurger", Dressing.TOMATO_SAUCE );
+      assertThat( burger1.hashCode() ).isNotEqualTo( burger2.hashCode() );
+   }
+
+   @Test
+   void shouldReturnMeaningfulToString() {
+      final Burger burger = new Burger( 1L, "TestBurger", Dressing.MUSTARD );
+      final String result = burger.toString();
+      assertThat( result ).contains( "Burger" );
+      assertThat( result ).contains( "TestBurger" );
+      assertThat( result ).contains( "MUSTARD" );
+      assertThat( result ).contains( "1" );
+   }
+
+   @Test
+   void shouldBuildBurgerWithBuilder() {
+      final Burger burger = Burger.builder()
+            .id( 5L )
+            .name( "BuilderBurger" )
+            .dressing( Dressing.CHILI_CHEESE_SAUCE )
+            .build();
+      assertThat( burger.getId() ).isEqualTo( 5L );
+      assertThat( burger.getName() ).isEqualTo( "BuilderBurger" );
+      assertThat( burger.getDressing() ).isEqualTo( Dressing.CHILI_CHEESE_SAUCE );
+   }
+
+   @Test
+   void shouldBuildBurgerWithDefaultValues() {
+      final Burger burger = Burger.builder().build();
+      assertThat( burger.getId() ).isZero();
+      assertThat( burger.getName() ).isNull();
+      assertThat( burger.getDressing() ).isNull();
+   }
+}
+
